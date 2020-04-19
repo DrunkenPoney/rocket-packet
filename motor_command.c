c9d8f68dd98f150754ab93e9107ab73f6ae220c6
Maxime Guillemette <maxime_glmt@hotmail.ca>
2020-04-19 11:19:13 -0400

    cleaned valid_function function

--- motor_command.c
+++ motor_command.c
@@ -10,17 +10,18 @@ int mc_valid_arg(int function, int arg) {
 	int valid;
 
 	switch (function) {
+	/* fallthrough */
 	case MC_SET_ACTUATOR:
-		/* fallthrough */
 	case MC_CLR_ACTUATOR:
 		/* argument is actuator id number */
 		valid = (0 <= arg) && (arg <= PISTON);
 		break;
+	/* fallthrough */
 	case MC_SET_ACTUATOR_MASK:
 	case MC_CLR_ACTUATOR_MASK:
 	case MC_SET_STATES:
 		/* argument is 6-bit mask */
-		valid = (0 <= arg) && (arg <= 0b00111111);
+		valid = (0b11000000 & arg) == 0;
 		break;
 	}
 
